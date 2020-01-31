8b54f9fe4dc8b97ab48afd8bd8ed29d5bf78b5a6
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2020-01-31 04:34:23 -0600

    Lesson 01 Stuff

---
+++ References/Proj_Used_For_Slides_Spring2020/Assets/Unit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour
+{
+    NavMeshAgent myAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+
+        Vector3 destination = new Vector3(9.5f, 1, -13);
+
+        myAgent.SetDestination(destination);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
