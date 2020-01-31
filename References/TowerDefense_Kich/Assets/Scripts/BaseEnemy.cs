ee00c176a29aad0bdc263c31edf8ebeefe91649d
Kichlids <kichangsongks@gmail.com>
2020-01-31 13:31:11 -0600

    Added enemy base class

---
+++ References/TowerDefense_Kich/Assets/Scripts/BaseEnemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class BaseEnemy : MonoBehaviour, IDamageable
+{
+    protected int maxHealth = 10;
+    protected int currentHealth;
+    protected float speed = 1f;
+
+    protected Player player;
+    protected NavMeshAgent agent;
+    protected GameObject destination;
+
+    protected bool isDead;
+
+    protected void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("GameManager").GetComponent<Player>();
+        agent = GetComponent<NavMeshAgent>();
+        destination = GameObject.FindGameObjectWithTag("Destination");
+
+        agent.SetDestination(destination.transform.position);
+        
+        isDead = false;
+    }
+
+
+    protected bool HasReachedDestination()
+    {
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            isDead = true;
+        }
+    }
+}
---
+++ References/TowerDefense_Kich/Assets/Scripts/Enemy1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1 : BaseEnemy
+{
+    private new void Start()
+    {
+        base.Start();
+
+        maxHealth = 5;
+        currentHealth = maxHealth;
+        speed = 10f;
+        agent.speed = speed;
+    }
+
+    private void Update()
+    {
+        if (isDead)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        if (HasReachedDestination())
+        {
+            Destroy(gameObject);
+            player.TakeDamage(currentHealth);
+            return;
+        }
+    }
+}
