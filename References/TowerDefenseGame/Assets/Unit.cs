d40c4d6740f9f3a46f83312aa04219aa0abe0606
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2020-02-11 22:13:43 -0600

    Everything in Lesson 01

---
+++ References/TowerDefenseGame/Assets/Unit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour
+{
+    NavMeshAgent myAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent = GetComponent<NavMeshAgent>();
+
+        myAgent.SetDestination(new Vector3(-5, 0, -13));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
